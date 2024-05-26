8f706d1cfdb8ff6dd0df6e18d834f19b4a24ba5a
Pixenal <pixenalws@gmail.com>
2024-05-25 18:32:28 -0700

    Setup basic macos support

--- RUVM_CLib.py
+++ RUVM_CLib.py
@@ -3,6 +3,8 @@ import sys
 
 if sys.platform == "win32":
     ruvmLibPath = "T:/workshop_folders/RUVM_Blender/Build/WinRelease/Debug/RUVMBlender.dll"
+elif sys.platform == "darwin":
+    ruvmLibPath = "/Users/calebdawson/Repos/RUVM_Blender/build/macosShared/libRUVMBlender.dylib"
 elif sys.platform == "linux" or "linux2":
     ruvmLibPath = "/run/media/calebdawson/Tuna/workshop_folders/RUVM_Blender/Build/Debug/libRUVMBlender.so"
 ruvmLib = ctypes.cdll.LoadLibrary(ruvmLibPath)

99f29d3878d0182311cf5af616dff5a0902ddefe
Pixenal <45414152+Pixenal@users.noreply.github.com>
2026-01-09 01:26:04 +1100

    continue with stuc mesh rendering

--- shaders/stuc_frag.glsl
+++ shaders/stuc_frag.glsl
@@ -1,6 +1,7 @@
 #define PI 3.14159265359
 #define MIN 1.0e-6
-#define SPEC_SAMPLES 64
+#define SPEC_SAMPLE_COUNT 64
+#define DIFF_SAMPLE_COUNT 128
 
 float geoSchlickGgx(float nov, float a) {
 	float a2 = (a * a) / 2.0f;
@@ -43,7 +44,6 @@ vec3 sampleEnvSpec(
 	vec3 brdf = f0 * g / denom;
 
 	vec3 light = textureLod(envTex, dirToUv(l), mip).xyz * 4.0f;
-
 	return brdf * light * nol;
 }
 
@@ -61,25 +61,20 @@ vec2 hammersley2d(uint i, uint num) {
 	return vec2(float(i) / float(num), radicalInvVdc(i));
 }
 
-uint fnvHash(uint value, uint size) {
-	uint hash = 2166136261;
-	for (int i = 0; i < 4; ++i) {
-		uint byte = (value >> 8 * i) & 0xFF;
-		hash ^= byte;
-		hash *= 16777619;
-	}
-	return hash % size;
+//https://jcgt.org/published/0009/03/02/paper.pdf
+//'pseudo' - pcg based, but we're not strictly adhereing to the spec
+uvec3 pcg3dPseudo(uvec3 value) {
+	value = value * 1664525u + 1013904223u;
+	value += uvec3(value.y * value.z, value.z * value.x, value.x * value.y);
+	value ^= value >> 16u;
+	value += uvec3(value.y * value.z, value.z * value.x, value.x * value.y);
+	return value;
 }
 
-float randFromDir(vec3 dir) {
-	vec3 dirAbs = abs(dir);
+vec3 randFromVec(vec3 vec) {
 	const uint iSize= 1048583u;
 	const float fSize = float(iSize);
-	uint iRand =
-		fnvHash(uint(dirAbs.x * fSize) * iSize, iSize) +
-		fnvHash(uint(dirAbs.y * fSize) * iSize, iSize) +
-		fnvHash(uint(dirAbs.z * fSize) * iSize, iSize);
-	return float(iRand % iSize) / fSize * 2.0f * float(PI);
+	return vec3(pcg3dPseudo(uvec3(vec * fSize)) % iSize) / fSize;
 }
 
 mat3 matrixForN(vec3 t, vec3 b, vec3 n) {
@@ -88,44 +83,60 @@ mat3 matrixForN(vec3 t, vec3 b, vec3 n) {
 	return mat3(right, left, n);
 }
 
-vec3 calcLights(vec3 v, mat3 tbn, vec3 n, vec3 albedo, float metal, float a) {
-	vec3 r = reflect(-v, n);
+const float mipTable[11] = float[11](
+	0.0f,
+	1.0f,
+	3.0f,
+	4.0f,
+	4.0f,
+	4.0f,
+	5.0f,
+	5.0f,
+	6.0f,
+	6.0f,
+	6.0f
+);
+
+vec3 calcLights(vec3 viewUvw, vec3 v, mat3 tbn, vec3 n, vec3 albedo, float metal, float a) {
 	mat3 nMat = matrixForN(tbn[0], tbn[1], n);
 	vec3 light = vec3(.0f);
-	float halfPi = PI / 2.0f;
-	float rand = randFromDir(r);
+	vec3 rand = randFromVec(abs(viewUvw + v));
+	a = clamp(a, .01f, .99f);
 	float a2 = a * a;
 
-	float mip = a >= 1.0f ? 100.0f : -2.0f / log(a) - 1.0f;
-	mip = clamp(mip, .0f, 6.0f);
-	mip = .0f;
+	const int ia = int(a * 10.0f);
+	const float mip = mix(mipTable[ia], mipTable[ia + 1], a * 10.0f - float(ia));
 
-	vec3 fDiff = fresnelSchlick(
-		mix(vec3(.04f), albedo, metal),
-		max(dot(n, v), .0f)
-	);
-
-	for (int i = 0; i < SPEC_SAMPLES; ++i) {
-		vec2 xi = hammersley2d(i + 1, SPEC_SAMPLES + 1);
-		float phi = xi.y * 2.0f * PI + rand;
-
-		float cosTheta = sqrt((1.0f - xi.x) / (1.0f + (a2 * a2 - 1.0f) * xi.x));
-		float sinTheta = sqrt(1.0f - cosTheta * cosTheta);
+	//float mip = a >= 1.0f ? 100.0f : -2.0f / log(a);
+	//mip = clamp(mip, .0f, 6.0f);
+    
+	vec3 specSum = vec3(.0f);
+    vec3 diffSum = vec3(.0f);
+	for (int i = 0; i < SPEC_SAMPLE_COUNT; ++i) {
+		vec2 xi = hammersley2d(uint(i + 1), uint(SPEC_SAMPLE_COUNT + 1));
+		xi += vec2(rand.x * -.125f, rand.y);
+		float phi = (xi.y) * 2.0f * PI;
+		const float cosTheta = sqrt((1.0f - xi.x) / (1.0f + (a2 * a2 - 1.0f) * xi.x));
+		const float sinTheta = sqrt(1.0f - cosTheta * cosTheta);
 		vec3 h = vec3(cos(phi) * sinTheta, sin(phi) * sinTheta, cosTheta);
 		h = nMat * normalize(h);
-		vec3 lSpec = reflect(-v, h);
-
-		cosTheta = 1.0f - xi.x;
-		sinTheta = sqrt(1.0f - cosTheta * cosTheta);
-		vec3 lDiff = vec3(cos(phi) * sinTheta, sin(phi) * sinTheta, cosTheta);
-		lDiff = nMat * normalize(lDiff);
-
-		vec3 spec = sampleEnvSpec(h, v, lSpec, n, albedo, metal, a2, mip);
-		vec3 diff = textureLod(envTex, dirToUv(lDiff), mip).xyz * 4.0f * max(dot(n, lDiff), .0f);
-		diff = diff * albedo * (1.0f - metal);
-		light += spec + diff;
+		const vec3 lSpec = reflect(-v, h);
+		specSum += sampleEnvSpec(h, v, lSpec, n, albedo, metal, a2, mip);
 	}
-	return light / float(SPEC_SAMPLES);
+	for (int i = 0; i < DIFF_SAMPLE_COUNT; ++i) {
+		vec2 xi = hammersley2d(uint(i + 1), uint(DIFF_SAMPLE_COUNT + 1));
+		xi += vec2(rand.z * -.125f, rand.x);
+		const float phi = (xi.y) * 2.0f * PI;
+		const float cosTheta = 1.0f - xi.x;
+		const float sinTheta = sqrt(1.0f - cosTheta * cosTheta);
+		vec3 lDiff = vec3(cos(phi) * sinTheta, sin(phi) * sinTheta, cosTheta);
+		lDiff = nMat * lDiff;
+		const float nol = max(dot(n, lDiff), .0f);
+		diffSum += textureLod(envTex, dirToUv(lDiff), 5.0f).xyz * 2.5f * nol;
+	}
+	return
+        diffSum / float(DIFF_SAMPLE_COUNT) * albedo * (1.0f - metal) +
+        specSum / float(SPEC_SAMPLE_COUNT);
 }
 
 vec3 v3SwizzleChannel(vec4 vec, int channel) {
@@ -171,7 +182,7 @@ void main() {
 	float rough = fSwizzleChannel(texture(roughTex, v_uv), int(matInfo.roughChannel));
 	rough = mix(matInfo.roughUniform, rough, matInfo.roughUseTex);
 
-	vec3 col = calcLights(v, m_tbn, normal, albedo, metal, rough);
+	vec3 col = calcLights(vec3(gl_FragCoord.xy, gl_FragDepth), v, m_tbn, normal, albedo, metal, rough);
 
 	//col = normalizeToRange(log2(col / .18), -10, 15);
 	//col = texture(tmLut, col).xyz;
